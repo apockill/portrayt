ee91d340ed9e013f905eeefd717a71fa3a5a082f
Alex Thiele <apocthiel@gmail.com>
2022-09-10 17:15:45 -0700

    Add rendering support

--- portrayt/main.py
+++ portrayt/main.py
@@ -1,8 +1,9 @@
 from argparse import ArgumentParser
 from pathlib import Path
 
-from portrayt.configuration import Configuration, PromptGenerateVariations
+from portrayt.configuration import Configuration, PromptGenerateVariations, RendererParams
 from portrayt.interface import MainApp
+from portrayt.renderers import RendererType
 
 
 def main() -> None:
@@ -14,6 +15,14 @@ def main() -> None:
         default=".portrayt-state",
         help="Where to store portrayt configuration and cached images",
     )
+    parser.add_argument(
+        "-r",
+        "--renderer",
+        required=True,
+        type=RendererType,
+        help=f"What type of renderer to use. Valid values are: {[v.value for v in RendererType]}",
+    )
+    parser.add_argument("-p", "--port", type=int, default=80)
     args = parser.parse_args()
 
     # Create necessary files and directories for usage
@@ -24,16 +33,25 @@ def main() -> None:
         config = Configuration(
             current_prompt_type=PromptGenerateVariations.__name__,
             prompt_generate_variations=PromptGenerateVariations(
-                prompt="Robots rights protest, vintage associated press photo",
+                prompt="Robots rights protest, colorized vintage newspaper scan",
                 num_variations=10,
             ),
-            portrait_width=640,
-            portrait_height=400,
+            renderer=RendererParams(seconds_between_image=30),
+            portrait_width=768,
+            portrait_height=512,
+            seed=1337,
         )
         config_path.write_text(config.json())
 
-    app = MainApp(configuration_path=config_path, cache_root_path=data_dir)
+    app = MainApp(
+        configuration_path=config_path,
+        cache_root_path=data_dir,
+        render_type=args.renderer,
+        port=args.port,
+    )
     app.launch()
+
+    app.close()
 
 
 if __name__ == "__main__":
