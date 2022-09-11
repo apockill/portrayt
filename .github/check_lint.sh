f4fefa2e060c8c0dbca24dfde630a35f77449dba
Alex Thiele <apocthiel@gmail.com>
2022-09-10 17:15:16 -0700

    Increase severity level for bandit

--- .github/check_lint.sh
+++ .github/check_lint.sh
@@ -6,6 +6,6 @@ poetry run mypy --ignore-missing-imports portrayt/ tests/
 poetry run isort --check --diff portrayt/ tests/
 poetry run black --check portrayt/ tests/
 poetry run flake8 portrayt/ tests/ --darglint-ignore-regex '^test_.*'
-poetry run bandit -r --severity medium high portrayt/ tests/
+poetry run bandit -r --severity high portrayt/ tests/
 poetry run vulture --min-confidence 100 portrayt/ tests/
 echo "Lint successful!"
